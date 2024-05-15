def38aa62e91a46a99336ae8def108e55f3b0525
Atomlayer <anfulu36484@gmail.com>
2024-05-15 22:02:07 +0300

    code refactor

---
+++ generator_base.py
@@ -0,0 +1,24 @@
+import abc
+
+
+class GeneratorBase(abc.ABC):
+
+    @abc.abstractmethod
+    def initialize(self):
+        pass
+
+    @abc.abstractmethod
+    def generate_op_post_topics(self, board_name):
+        pass
+
+    @abc.abstractmethod
+    def generate_op_post(self, op_post_topic):
+        pass
+
+    @abc.abstractmethod
+    def generate_new_posts(self, posts):
+        pass
+
+    @abc.abstractmethod
+    def generate_image_prompt(self, message):
+        pass
